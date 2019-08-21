b420f3a2c3ab03ebce520c13c15ae657e1204a55
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2019-08-21 17:53:50 -0300

    Aula 107 Teste polimorf linha 51 program103.java

--- Aula103Heranca/application/Program103.java
+++ Aula103Heranca/application/Program103.java
@@ -1,7 +1,6 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program103 {
@@ -37,7 +36,7 @@ public class Program103 {
 			System.out.println("Update");
 		}*/
 		
-		Account acc1 = new Account(1001, "Alex", 1000.0);
+		/*Account acc1 = new Account(1001, "Alex", 1000.0);
 		acc1.withdraw(200);
 		System.out.println(acc1.getBalance());
 		
@@ -47,7 +46,17 @@ public class Program103 {
 		
 		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 		acc3.withdraw(200);
-		System.out.println(acc3.getBalance());
+		System.out.println(acc3.getBalance());*/
+		
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		
+		x.withdraw(50);
+		y.withdraw(50);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+		
 	}
 
 }
