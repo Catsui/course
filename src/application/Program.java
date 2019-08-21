2455ed17657fa78b74df1eb3c6a2ea0f8faca585
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2019-08-21 11:17:06 -0300

    Aula 97

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ import entities.enums.OrderStatus;
 public class Program {
 
 	public static void main(String[] args) {
-		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		/*Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 		
 		System.out.println(order);
 		
@@ -16,7 +16,9 @@ public class Program {
 		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
 		
 		System.out.println(os1);
-		System.out.println(os2);
+		System.out.println(os2);*/
+		
+		
 
 	}
 
