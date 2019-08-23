6ed3d2db2cc0cb04e76eab1c23d97d490ceae377
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2019-08-23 09:00:06 -0300

    Aula 163 depois de reabastecer. Checkpoint.

--- Aula163/application/Program163.java
+++ Aula163/application/Program163.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
+import model.entities.Product163;
 
 public class Program163 {
 	
@@ -15,28 +20,49 @@ public class Program163 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		List<Product> products = new ArrayList<>();
+		List<Product163> products = new ArrayList<>();
 				
 		System.out.print("Enter a file path: ");
-		String strPath = sc.nextLine();
+		String sourceStr = sc.nextLine();
 		
-		File source = new File(strPath);
+		File source = new File(sourceStr);
 		String sourcePath = source.getParent();
 		
-		boolean success
+		boolean success = new File(sourcePath + "\\out").mkdir();
 		
-		File path = new File(strPath);
+		String targetStr = sourcePath + "\\out\\summary.csv";
 		
-		String name = sc.nextLine();
-		double price = sc.nextDouble();
-		int quantity = sc.nextInt();
+		try (BufferedReader br = new BufferedReader(new FileReader(source))) {
+			
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+				
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+				
+				products.add(new Product163(name, price, quantity));
+				itemCsv = br.readLine();
+			}
+			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetStr))){
+				for (Product163 product:products) {
+					bw.write(product.getName() + "," + String.format("%.2f", product.totalValue()));
+					bw.newLine();
+				}
+				System.out.println(targetStr + " created.");
+			}
+			catch (IOException e) {
+				e.printStackTrace();		
+			} 	
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		
-		
-		System.out.println("getName: " + path.getName());
-		System.out.println("getParent: " + path.getParent());
-		System.out.println("getPath: " + path.getPath());
-		
+		if (success) System.out.println();				
+	
 		sc.close();
 	}
 }
