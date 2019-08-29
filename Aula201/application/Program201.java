6ba944accd7e11b795f7b5582aba32fcbed1038e
Ivander Salvador Ruiz <ivandersalvadorruiz@gmail.com>
2019-08-29 19:00:24 -0300

    Aula 201

---
+++ Aula201/application/Program201.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product195;
+
+public class Program201 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+					
+		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ivand\\eclipse-workspace\\course\\Aula201\\in.txt"))){
+			List<Product195> products = new ArrayList<>();
+			String line = br.readLine();
+			while (line!=null) {
+				String[] fields = line.split(",");
+				products.add(new Product195(fields[0],Double.parseDouble(fields[1])));				
+				line = br.readLine();
+			}
+			
+			double avg = products.stream()
+					.map(x->x.getPrice())
+					.reduce(0.,(x,y)->x+y)/products.size();
+			
+			System.out.println("Average price: " + String.format("%.2f",avg));
+			
+			Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> names = products.stream()
+					.filter(p -> p.getPrice()<avg)
+					.map(p -> p.getName())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out::println);
+					
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
